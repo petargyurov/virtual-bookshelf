a67c73af42685260577ec9485d5cc366b244756b
Petar Gyurov <pgyurov93@gmail.com>
2022-05-02 16:37:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Virtual Bookshelf
+This is a simple visualisation of books on a bookshelf using some CSS transforms to give the effect of picking out the book when you hover over it.
+
+I use it on my personal site to track what books I've read. It integrates nicely with static site generators, and well, just about anything since it's all vanilla JS, CSS and HTML.
+
+![Example](https://imgur.com/a/nIYFQA5)
+
+*Why use JS at all?*
+I originally aimed for a no-JS implementation but there was no way of adding randomness to the book height, colours and patterns without it. You can of course remove the JS and implement that stuff manually.
+
+*Is this free to use?*
+Yep, do whatever you want with it.
+
+*Is it perfect?*
+Nope. Doesn't handle long titles. I'm sure there are other alignment issues. I wrote this in a day, don't expect much.
---
+++ bookshelf.js
@@ -0,0 +1,42 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomChoice(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+let spines = Object.values(document.getElementsByClassName("spine"));
+let covers = Object.values(document.getElementsByClassName("cover"));
+let tops = Object.values(document.getElementsByClassName("top"));
+
+let availablePatterns = ["argyle", "tartan"]; // we could probably get these programatically
+let availableColours = [
+  "maroon",
+  "darkgreen",
+  "darkolivegreen",
+  "brown",
+  "saddlebrown",
+  "sienna",
+  "midnightblue",
+];
+
+// assign a random height, pattern and colour to each book
+spines.map(function (s, i) {
+  let randomHeight = getRandomInt(220, 290);
+  s.style.height = `${randomHeight}px`;
+  s.style.top = `${280 - randomHeight}px`;
+
+  let randomPattern = randomChoice(availablePatterns);
+  s.style.backgroundImage = `var(--${randomPattern})`;
+
+  let randomColor = randomChoice(availableColours);
+  s.style.backgroundColor = randomColor;
+
+  covers[i].style.height = `${randomHeight}px`;
+  covers[i].style.top = `${280 - randomHeight}px`;
+
+  tops[i].style.top = `${280 - randomHeight}px`;
+});

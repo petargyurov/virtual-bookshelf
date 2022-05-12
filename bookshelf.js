1bcbfb2558c7774077ec017bc7fd7766359f464a
marlonmountjoy <mmountjoy@spscc.edu>
2022-05-11 18:32:43 -0700

    This is for Pull

--- bookshelf.js
+++ bookshelf.js
@@ -1,47 +1,66 @@
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function randomChoice(array) {
-  return array[Math.floor(Math.random() * array.length)];
+    return array[Math.floor(Math.random() * array.length)];
 }
 
-let spines = Object.values(document.getElementsByClassName("spine"));
-let covers = Object.values(document.getElementsByClassName("cover"));
-let tops = Object.values(document.getElementsByClassName("top"));
 
-let availablePatterns = ["argyle", "tartan"]; // we could probably get these programatically
-let availableColours = [
-  "maroon",
-  "darkgreen",
-  "darkolivegreen",
-  "brown",
-  "saddlebrown",
-  "sienna",
-  "midnightblue",
-];
+function identity() { // wrapped authors original code in a function in order to call it on the inserted book divs
+    let spines = Object.values(document.getElementsByClassName("spine"));
+    let covers = Object.values(document.getElementsByClassName("cover"));
+    let tops = Object.values(document.getElementsByClassName("top"));
 
-// assign a random height, pattern and colour to each book
-spines.map(function (s, i) {
-  let randomHeight = getRandomInt(220, 290);
-  s.style.height = `${randomHeight}px`;
-  s.style.top = `${280 - randomHeight}px`;
+    let availablePatterns = ["argyle", "tartan"]; // we could probably get these programmatically
+    let availableColors = [
+        "maroon",
+        "darkgreen",
+        "darkolivegreen",
+        "brown",
+        "saddlebrown",
+        "sienna",
+        "midnightblue",
+    ];
 
-  let randomPattern = randomChoice(availablePatterns);
-  s.style.backgroundImage = `var(--${randomPattern})`;
+    // assign a random height, pattern and color to each book
+    spines.map(function (s, i) {
+        let randomHeight = getRandomInt(220, 290);
+        s.style.height = `${randomHeight}px`;
+        s.style.top = `${280 - randomHeight}px`;
 
-  let randomColor = randomChoice(availableColours);
-  s.style.backgroundColor = randomColor;
+        let randomPattern = randomChoice(availablePatterns);
+        s.style.backgroundImage = `var(--${randomPattern})`;
 
-  covers[i].style.height = `${randomHeight}px`;
-  covers[i].style.top = `${280 - randomHeight}px`;
+        let randomColor = randomChoice(availableColors);
+        s.style.backgroundColor = randomColor;
 
-  tops[i].style.top = `${280 - randomHeight}px`;
-});
+        covers[i].style.height = `${randomHeight}px`;
+        covers[i].style.top = `${280 - randomHeight}px`;
 
-function addBook(author, title) {
-    var newDiv = document.querySelector('div.book')
-    newDiv.insertAdjacentHTML('afterend','<div class="book"><div class="side spine"><span class="spine-title"> Book Title </span><span class="spine-author"> PG </span></div></div>')
+        tops[i].style.top = `${280 - randomHeight}px`;
+    });
 }
+function add() { // This is by MarMar using the authors original open license book div code as bookDetails feel free to use
+    let cover = document.getElementById('txtCover').value;
+    let title = document.getElementById('txtTitle').value;// Get value from text box
+    let author = document.getElementById('txtAuthor').value;
+    let book = document.createElement('div');// create new div element
+    // assign original authors book div code to bookDetails
+    let bookDetails = `<div class="book"> 
+  <div class="side spine">
+      <span class="spine-title">${title}</span>
+      <span class="spine-author">${author}</span>
+  </div>
+  <div class="side top"></div>
+  <div class="side cover"style="background-image: url(${cover})"></div>
+  
+  </div>`;
+    book.insertAdjacentHTML('beforeend', bookDetails);// insert bookDetails into book div
+    let bookshelf = document.querySelector('.bookshelf');// find bookshelf div by class
+    bookshelf.appendChild(book);// add book in bookshelf div
+    this.identity();// run authors original randomizer script repackaged as identity() on the new book
+}
+
